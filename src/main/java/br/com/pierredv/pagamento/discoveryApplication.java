c8019164df13a41975f6f29ee0a1852c475a9897
Pierre enoc <pierre.enoc@gympass.com>
2021-02-22 00:28:04 -0300

    created eureka server

---
+++ src/main/java/br/com/pierredv/pagamento/discoveryApplication.java
@@ -0,0 +1,15 @@
+package br.com.pierredv.pagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class discoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(discoveryApplication.class, args);
+	}
+
+}
